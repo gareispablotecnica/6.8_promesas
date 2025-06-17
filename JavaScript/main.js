91eaabaf315976393c89823263c5f092fc726dec
Pablo Gareis <gareispablotecnica1@gmail.com>
2025-06-17 16:08:18 -0300

    addEventListener

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,28 +1,34 @@
-import {Usuarios} from './arrays.js'
+import { Usuarios } from './arrays.js'
 
-let Nuevousuario=prompt("Ingrese Nombre del Usuario: ")
 
-function buscar(){
-    return new Promise((resolve,reject)=>{
-        if(Usuarios==0){
+function buscar() {
+
+    let Nuevousuario = document.getElementById('user').value.trim()
+
+    return new Promise((resolve, reject) => {
+        if (Usuarios == 0) {
             reject(new Error('❗ Api no Encontrada'));
         }
-        
-        const busqueda= Usuarios.find(i=> i.Usuario.toLowerCase() === Nuevousuario.toLowerCase())
-        
+
+        const busqueda = Usuarios.find(i => i.Usuario.toLowerCase() === Nuevousuario.toLowerCase())
+
         // --> Luego de 3s mostramos el arrays
         setTimeout(() => {
-           busqueda ? resolve(busqueda) : reject('Usuario No Encontrado')
+            busqueda ? resolve(busqueda) : reject('Usuario No Encontrado')
         }, 3000);
     })
 }
 
-console.log('🔎 Buscando...')
-buscar()
-.then((response)=>{
-    console.clear()
-    console.log("✔ Arrays Encontrado: ",response)
+
+document.getElementById('btnbuscar').addEventListener('click',()=> {
+    console.log('🔎 Buscando...')
+    buscar()
+    .then((response) => {
+            console.clear()
+            console.log("✔ Arrays Encontrado: ", response)
+        })
+    .catch((Error) => {
+            console.log("Error en:", Error)
+    })
 })
-.catch((Error)=>{
-    console.log("Error en:",Error)
-})
+
